60e7b4f7a86ffe9d054228983adaf2dde03e999d
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-02-26 07:46:06 -0300

    Updated language list and XMLJava

--- src/com/maxprograms/languages/Constants.java
+++ src/com/maxprograms/languages/Constants.java
@@ -17,6 +17,6 @@ public class Constants {
         // do not instantiate
     }
     
-    public static final String VERSION = "1.0.3";
-    public static final String BUILD = "20241125_0627";
+    public static final String VERSION = "1.0.4";
+    public static final String BUILD = "20250226_0742";
 }
