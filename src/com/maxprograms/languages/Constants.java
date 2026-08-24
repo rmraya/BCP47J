edf7d458be048ea95c0029d9fd70a7322840ab5c
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-08-24 10:56:30 -0300

    Added en-IQ to extended language list

--- src/com/maxprograms/languages/Constants.java
+++ src/com/maxprograms/languages/Constants.java
@@ -15,6 +15,6 @@ public class Constants {
         // do not instantiate
     }
     
-    public static final String VERSION = "4.2.0";
-    public static final String BUILD = "20260725_1521";
+    public static final String VERSION = "4.2.1";
+    public static final String BUILD = "20260824_1055";
 }
